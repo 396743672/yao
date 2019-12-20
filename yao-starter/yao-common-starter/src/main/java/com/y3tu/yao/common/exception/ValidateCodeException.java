44408ceecfc91c987a2280bdc8aa7dccd196b30e
y3tu <14643092@qq.com>
2019-12-20 22:05:40 +0800

    update

---
+++ yao-starter/yao-common-starter/src/main/java/com/y3tu/yao/common/exception/ValidateCodeException.java
@@ -0,0 +1,13 @@
+package com.y3tu.yao.common.exception;
+
+/**
+ * 验证码类型异常
+ *
+ * @author y3tu
+ */
+public class ValidateCodeException extends Exception {
+
+    public ValidateCodeException(String message) {
+        super(message);
+    }
+}
---
+++ yao-starter/yao-common-starter/src/main/java/com/y3tu/yao/common/handler/ServerAccessDeniedHandler.java
@@ -0,0 +1,16 @@
+package com.y3tu.yao.common.handler;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServerAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+
+    }
+}
